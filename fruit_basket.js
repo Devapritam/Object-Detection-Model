0ec8dd7cff174f052e628a5874afcff7c576e11f
Devapritam Sharma <dsarma1980@gmail.com>
2021-10-06 16:47:56 +0530

    Add files via upload

---
+++ fruit_basket.js
@@ -0,0 +1,14 @@
+img = "";
+
+function preload() {
+    img = loadImage("fruit basket.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(540, 320);
+    canvas.center();
+}
+
+function draw() {
+    image(img, 0, 0, 540, 320);
+}
