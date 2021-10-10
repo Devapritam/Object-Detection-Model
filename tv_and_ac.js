f6f4582da17dffaae6ee5939b274da8b7e44c5d9
Devapritam Sharma <dsarma1980@gmail.com>
2021-10-10 16:23:45 +0530

    Add files via upload

--- tv_and_ac.js
+++ tv_and_ac.js
@@ -1,4 +1,5 @@
 img = "";
+status = "";
 
 function preload() {
     img = loadImage("tv and ac.jpg");
@@ -7,6 +8,23 @@ function preload() {
 function setup() {
     canvas = createCanvas(540, 320);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status - Detecting Objects";
+}
+
+function modelLoaded() {
+    window.alert("Cocossd model has been loaded successfully in your website");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+    }
 }
 
 function draw() {
